bf68a27befe408c67d89031d339999f8923f05e3
Alexey Ivashchenko <alex_kht@mail.ru>
2024-03-25 20:40:34 +1000

    Sprint_4 develop

---
+++ src/main/java/ru/praktikum_services/qa_scooter/EnvConfig.java
@@ -0,0 +1,6 @@
+package ru.praktikum_services.qa_scooter;
+
+public class EnvConfig {
+    public static final String BASE_URL = "https://qa-scooter.praktikum-services.ru/";
+    public static final int DEFAULT_TIMEOUT = 10;
+}
